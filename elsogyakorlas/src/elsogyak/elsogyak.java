8bb6e01c5dd9b48f5abc968a79f1137768b7740b
Kitti <Kitti@192.168.0.192>
2018-02-25 16:47:49 +0100

    Masodik feladatlaprol az otodik feladat.

---
+++ elsogyakorlas/src/elsogyak/elsogyak.java
@@ -0,0 +1,15 @@
+package elsogyak;
+
+public class elsogyak {
+
+	public static void main(String[] args) {
+		for (int i=0; i<=8; i++) {
+			System.out.print("\n");
+			for (int j=0; j<i; j++) {
+				System.out.print(i);
+			}
+		}
+
+	}
+
+}
